52ab70ad18b9b90e0f3f96fe52d10f07020d83ca
NippleLion <piolotorrico@yahoo.com>
2021-10-15 10:25:57 -0700

    User can now view their own posts in profile tab. User can see last 20 posts.

--- app/src/main/java/com/example/instagramclone/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagramclone/fragments/ProfileFragment.java
@@ -14,13 +14,19 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.instagramclone.LoginActivity;
+import com.example.instagramclone.Post;
 import com.example.instagramclone.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends PostsFragment {
 
     public static final String TAG = "ProfileFragment";
     private Button btnLogout;
@@ -60,4 +66,29 @@ public class ProfileFragment extends Fragment {
         Intent i = new Intent(getContext(), LoginActivity.class);
         startActivity(i);
     }
+
+    @Override
+    protected void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_KEY);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post: posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+
 }
